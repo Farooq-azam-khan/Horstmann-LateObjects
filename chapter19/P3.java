afd95a2e1f54fca4629118c0c6fb974dd2dd4e18
Farooq Azam Khan <farooq1.khan@ryerson.ca>
2018-04-23 00:20:42 -0400

    fixed vigenere decryption: added letter to decryption process

--- chapter19/P3.java
+++ chapter19/P3.java
@@ -15,17 +15,89 @@ the sixth letter of the input text is encrypted in the same way as the first.
 Write a program that encrypts or decrypts an input text using this cipher.
 Use command line arguments as in Exercise P19.1.
 */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import java.io.PrintWriter;
+
 public class P3
 {
   public static void main(String[] args)
   {
-    VigenereCipher cipher = new VigenereCipher("tiger");
-    System.out.println(cipher.encrypt("hello world"));
-    System.out.println(cipher.decrypt("amrpf pwxpu"));
+    if (args.length <4 || args.length > 5)
+    {
+      usage();
+      System.exit(0);
+    }
 
+    String inputFileName = "";
+    String outputFileName = "";
+    String key = "";
+    boolean encrypting = true;
 
+    if (args[0].equals("-d"))
+    {
+      if (args[1].equals("-k"))
+      {
+        key = args[2];
+        encrypting = false;
+        inputFileName = args[3];
+        outputFileName = args[4];
+      }
+    }
+    else if (args[0].equals("-k"))
+    {
+      key = args[1];
+      inputFileName = args[2];
+      outputFileName = args[3];
+    }
+
+    // System.out.println("key: " + key + " encrypting: " + encrypting + " inputFileName: " + inputFileName + " outputFileName: " + outputFileName);
+
+    try
+    {
+      Scanner in = new Scanner(new File(inputFileName));
+      PrintWriter out = new PrintWriter(outputFileName);
+
+      VigenereCipher cipher = new VigenereCipher("tiger");
+
+      if (encrypting)
+      {
+        while(in.hasNextLine())
+        {
+          // encrypt line by line
+          String encrypted_line = cipher.encrypt(in.nextLine());
+          out.println(encrypted_line);
+        }
+      }
+      else
+      {
+        while(in.hasNextLine())
+        {
+          // decrypt line by line
+          String decrypted_line = cipher.decrypt(in.nextLine());
+          out.println(decrypted_line);
+        }
+      }
+
+      in.close();
+      out.close();
+    }
+
+    catch(FileNotFoundException e)
+    {
+      System.out.println(e.getMessage());
+    }
+  }
+  public static void usage()
+  {
+    System.out.println("usage: java P3 [optional=-d] -k [key] [inputfile] [outputfile]");
   }
 }
+
+
 
 class VigenereCipher
 {
@@ -82,7 +154,7 @@ class VigenereCipher
       char base;
       char key_ch = key.charAt(key_counter%key.length()); // get the character for encryption
 
-      System.out.print("ch: " + ch + " key_ch: " + key_ch + " ");
+      // System.out.print("ch: " + ch + " key_ch: " + key_ch + " ");
       if (ch >= 'a' && ch <= 'z')
       {
         base = 'a';
@@ -101,7 +173,7 @@ class VigenereCipher
       {
         decrypted_msg += String.valueOf(ch);
 
-        System.out.println("");
+        // System.out.println("");
       }
 
     }
@@ -120,12 +192,10 @@ class VigenereCipher
   }
   private String getDecryptedLetter(char base, char key_ch, char ch)
   {
-    // TODO : FIX DECRYPTION 
     int char_index = ch - base;
     int key_index = key_ch - base;
 
-    System.out.println("char_indx: " + char_index + " key_indx: " + key_index);
-    int decrypted_index = (Math.abs(char_index - key_index))%LETTERS;
+    int decrypted_index = (Math.abs(char_index - key_index + LETTERS))%LETTERS;
     char decrypted_char = (char)((decrypted_index)+base);
     return String.valueOf(decrypted_char);
   }
